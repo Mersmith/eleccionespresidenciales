9b33011c83f11c585e0324e146c8420870bf2aea
Mersmith <mersmith14@gmail.com>
2025-07-31 18:02:31 -0500

    avance

--- resources/views/livewire/web/encuesta/encuesta-resultado-livewire.blade.php
+++ resources/views/livewire/web/encuesta/encuesta-resultado-livewire.blade.php
@@ -3,41 +3,61 @@
 
         <div class="g_pading_pagina g_gap_pagina">
 
+            @php
+                $totalVotos = $resultados->sum('votos');
+            @endphp
+
+            <div class="titulares">
+                <h4 class="g_texto_nivel_6">ENCUESTA</h4>
+                {{-- <h3 class="g_texto_nivel_6">{{ $encuesta->nombre }} </h3> --}}
+                <p class="g_texto_nivel_2">
+                    <strong>Inicio:</strong> {{ $encuesta->fecha_inicio_formateada }} |
+                    <strong>Fin:</strong> {{ $encuesta->fecha_fin_formateada }}
+                </p>
+                <p class="g_texto_nivel_1"><i class="fas fa-user-tie"></i>
+                    {{ $encuesta->cargo->nombre ?? '-' }}</p>
+
+                @php
+                    $ubicacion = collect([
+                        $encuesta->pais?->nombre,
+                        $encuesta->region?->nombre,
+                        $encuesta->provincia?->nombre,
+                        $encuesta->distrito?->nombre,
+                    ])
+                        ->filter()
+                        ->join(' / ');
+                @endphp
+
+                <span class="g_texto_nivel_3">
+                    <i class="fas fa-map-marker-alt"></i> {{ $ubicacion }}
+                </span>
+            </div>
+
+            <div class="grafico_vertical">
+                <p class="g_texto_nivel_3">Total de votos: <strong>{{ $totalVotos }}</strong></p>
+
+                <div class="grafico_barras_scroll g_scroll">
+                    <div class="grafico">
+                        @foreach ($resultados as $item)
+                            @php
+                                $porcentaje = $totalVotos > 0 ? round(($item['votos'] / $totalVotos) * 100, 2) : 0;
+                            @endphp
+                            <div class="barra_container">
+                                <div class="barra" style="height: {{ $porcentaje }}%">
+                                    <img src="{{ $item['partido_foto'] }}" class="logo" alt="Partido">
+                                </div>
+                            </div>
+                        @endforeach
+                    </div>
+                </div>
+            </div>
+
             <div class="g_grid_pagina_2_columnas">
                 <!-- COLUMNA 1 -->
                 <div class="g_grid_columna_1">
 
-                    @php
-                        $totalVotos = $resultados->sum('votos');
-                    @endphp
-
                     <div class="partials_contendor_encuesta">
-                        <div class="titulares">
-                            <h4 class="g_texto_nivel_6">ENCUESTA</h4>
-                            {{-- <h3 class="g_texto_nivel_6">{{ $encuesta->nombre }} </h3> --}}
-                            <p class="g_texto_nivel_2">
-                                <strong>Inicio:</strong> {{ $encuesta->fecha_inicio_formateada }} |
-                                <strong>Fin:</strong> {{ $encuesta->fecha_fin_formateada }}
-                            </p>
-                            <p class="g_texto_nivel_1"><i class="fas fa-user-tie"></i>
-                                {{ $encuesta->cargo->nombre ?? '-' }}</p>
-
-                            @php
-                                $ubicacion = collect([
-                                    $encuesta->pais?->nombre,
-                                    $encuesta->region?->nombre,
-                                    $encuesta->provincia?->nombre,
-                                    $encuesta->distrito?->nombre,
-                                ])
-                                    ->filter()
-                                    ->join(' / ');
-                            @endphp
-
-                            <span class="g_texto_nivel_3">
-                                <i class="fas fa-map-marker-alt"></i> {{ $ubicacion }}
-                            </span>
-                        </div>
-
+                        
                         <div class="grafico_horizontal">
                             <p class="g_texto_nivel_3">Total de votos: <strong>{{ $totalVotos }}</strong></p>
 
@@ -63,27 +83,6 @@
                                     </div>
                                 </div>
                             @endforeach
-                        </div>
-
-
-                        <div class="grafico_vertical">
-                            <p class="g_texto_nivel_3">Total de votos: <strong>{{ $totalVotos }}</strong></p>
-
-                            <div class="grafico_barras_scroll">
-                                <div class="grafico">
-                                    @foreach ($resultados as $item)
-                                        @php
-                                            $porcentaje =
-                                                $totalVotos > 0 ? round(($item['votos'] / $totalVotos) * 100, 2) : 0;
-                                        @endphp
-                                        <div class="barra_container">
-                                            <div class="barra" style="height: {{ $porcentaje }}%">
-                                                <img src="{{ $item['partido_foto'] }}" class="logo" alt="Partido">
-                                            </div>
-                                        </div>
-                                    @endforeach
-                                </div>
-                            </div>
                         </div>
 
                         <a class="boton_siguiente"
